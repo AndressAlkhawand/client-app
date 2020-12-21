fcf93f3c973f0edb3743573fb1bed043abeb7d4c
Andress <andress.khawand@gmail.com>
2020-12-20 22:04:59 -0500

    Fixes

--- src/app/modules/notary-module/components/main-page/main-page.component.ts
+++ src/app/modules/notary-module/components/main-page/main-page.component.ts
@@ -2,23 +2,32 @@ import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import {MediaMatcher} from '@angular/cdk/layout';
 import { faHashtag } from '@fortawesome/free-solid-svg-icons';
 import { faSchool } from '@fortawesome/free-solid-svg-icons';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnDestroy {
-  title = 'My-App';
+export class MainPageComponent implements OnDestroy, OnInit {
   mobileQuery: MediaQueryList;
   instagram = faHashtag;
   facebook = faSchool;
   private _mobileQueryListener: () => void;
+  name: any;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+     media: MediaMatcher,
+     public router: Router,
+     public route: ActivatedRoute,
+     ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnInit() {
   }
 
   ngOnDestroy(): void {
@@ -30,7 +39,7 @@ export class MainPageComponent implements OnDestroy {
   }
 
   public navigate(destination: string): void {
-    
+    this.router.navigate(['about-us'], {relativeTo: this.route});
   }
 
 }
